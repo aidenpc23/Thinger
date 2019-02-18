a9ea99e4c780872bf884abd285edb2b2b435c69f
aidenpc23 <47757506+aidenpc23@users.noreply.github.com>
2019-02-18 14:35:44 -0500

    Add files via upload

--- UnThinger.bat
+++ UnThinger.bat
@@ -6,6 +6,7 @@ set /p message=Message:
 echo %message% > message.txt
 set /p password=Password:
 openssl enc -d -A -a -in message.txt -out enc.txt -k %password%
+type enc.txt
 pause
 del message.txt && del enc.txt
 
